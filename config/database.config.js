01cc23c30987115adda6182299e080878e9808b2
sauumy <saumyashukla96o@gmail.com>
2023-05-05 17:44:13 +0530

    DB Changes

--- config/database.config.js
+++ config/database.config.js
@@ -5,5 +5,7 @@
 // //mongodb://hardeep:yourSecretPassword@169.45.23.99/animals
 
 module.exports = {
-   url:'mongodb://212.47.226.119/Souliepie'
+   url:'mongodb://Soulipie:1234@212.47.226.119/Souliepie'
 }
+
+
