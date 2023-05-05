8794561872788093858352507a9f5f714cdd5465
sauumy <saumyashukla96o@gmail.com>
2023-05-05 17:36:14 +0530

    latest changes

--- config/database.config.js
+++ config/database.config.js
@@ -1,5 +1,9 @@
-module.exports = {
-   url:'mongodb://impana:hiHowareyou@15.206.13.51/Soulipie'
-}
+// module.exports = {
+//    url:'mongodb://impana:hiHowareyou@15.206.13.51/Soulipie'
+// }
 
-//mongodb://hardeep:yourSecretPassword@169.45.23.99/animals
+// //mongodb://hardeep:yourSecretPassword@169.45.23.99/animals
+
+module.exports = {
+   url:'mongodb://212.47.226.119/Souliepie'
+}
