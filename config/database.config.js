67b4202ffb0622072696cb830d5bd066b1325166
sauumy <saumyashukla96o@gmail.com>
2023-05-12 14:42:36 +0530

    db changed

--- config/database.config.js
+++ config/database.config.js
@@ -5,7 +5,7 @@
 // //mongodb://hardeep:yourSecretPassword@169.45.23.99/animals
 
 module.exports = {
-   url:'mongodb://impana:9799@212.47.226.119/Souliepie'
+   url:'mongodb://soulipie:1234@15.206.13.51/Souliepie'
 }
 
 
