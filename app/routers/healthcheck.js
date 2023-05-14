8d1118ef2ced3dd497492cdd3bd3c7ca4a5776ea
sauumy <saumyashukla96o@gmail.com>
2023-05-14 13:33:29 +0530

    Code Changes

--- app/routers/healthcheck.js
+++ app/routers/healthcheck.js
@@ -5,7 +5,7 @@ router.get('/', async (_req, res, _next) => {
     const healthcheck = {
         uptime: process.uptime(),
         message: 'OK',
-        timestamp: Date.now()
+        timestampp: Date.now()
     };
     try {
         res.send(healthcheck);
